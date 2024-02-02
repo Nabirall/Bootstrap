7c0e824804582d0c6703f16946b94bce5920bd79
Nabirall <drozdover99@mail.ru>
2024-02-02 10:46:42 +0300

    Initial commit

---
+++ src/main/java/demo/dao/RoleDao.java
@@ -0,0 +1,18 @@
+package demo.dao;
+
+
+import demo.model.Role;
+
+import java.util.List;
+
+public interface RoleDao {
+
+    List<Role> getAllRoles();
+
+    Role getRole(String userRole);
+
+    Role getRoleById(Long id);
+
+    void addRole(Role role);
+
+}
---
+++ src/main/java/demo/service/UserService.java
@@ -0,0 +1,20 @@
+package demo.service;
+
+
+import demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void saveUser(User user);
+
+    void updateUser(User user);
+
+    void deleteUser(long id);
+
+    List<User> getAllUsers();
+
+    User getUserByLogin(String login);
+
+    User getUserById(long id);
+}
